b38798594cc07be5a33c5a27c27c829a0be695ea
mattipunkt <mattihro105@gmail.com>
2025-03-22 19:52:56 +0100

    forgor

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,10 @@ cp .env.deploy .env
 echo "DB_USERNAME=$DB_USERNAME" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 echo "APP_URL=$APP_URL" >> .env # e.g. https://mydomain.com
-echo "SESSION_DOMAIN"="$SESSION_DOMAIN" >> .env  # e.g mydomain.com
+echo "SESSION_DOMAIN=$SESSION_DOMAIN" >> .env  # e.g mydomain.com
+echo "PROXY_URL=$PROXY_URL" >> .env
+echo "PROXY_SCHEMA=$PROXY_SCHEMA" >> .env
+
 
 php artisan key:generate
 php artisan config:clear
