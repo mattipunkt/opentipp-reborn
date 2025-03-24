e85a45f05fe97a564dc878ceef120fc855e3be56
mattipunkt <mattihro105@gmail.com>
2025-03-24 20:33:46 +0100

    .env updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,12 @@ echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 echo "APP_URL=$APP_URL" >> .env # e.g. https://mydomain.com
 echo "SESSION_DOMAIN=$SESSION_DOMAIN" >> .env  # e.g mydomain.com
 echo "PROXY_URL=$PROXY_URL" >> .env
-
+echo "MAIL_MAILER=$MAIL_MAILER" >> .env
+echo "MAIL_HOST=$MAIL_HOST" >> .env
+echo "MAIL_PORT=$MAIL_PORT" >> .env
+echo "MAIL_USERNAME=$MAIL_USERNAME" >> .env
+echo "MAIL_PASSWORD=$MAIL_PASSWORD" >> .env
+echo "MAIL_FROM_ADDRESS=$MAIL_FROM_ADDRESS" >> .env
 
 php artisan key:generate
 php artisan optimize:clear
