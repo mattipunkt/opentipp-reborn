b822ac9e0f5a2f42f908316e721edc1a69b2b3f2
mattipunkt <mattihro105@gmail.com>
2026-03-21 12:47:06 +0100

    html test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
         Blade::if('admin', function () {
             return Auth::check() && Auth::user()->is_admin;
         });
